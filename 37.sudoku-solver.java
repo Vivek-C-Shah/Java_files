3a7425bcdb4d140f23c987c710d674c7b009d9d3
V1v3kShah <94179705+Vivek-C-Shah@users.noreply.github.com>
2024-03-29 07:09:28 +0530

    sudoku solver done

---
+++ 37.sudoku-solver.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=37 lang=java
+ *
+ * [37] Sudoku Solver
+ */
+
+// @lc code=start
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.'; // backtrack
+                            }
+                        }
+                    }
+                    return false; // no valid number found
+                }
+            }
+        }
+        return true; // sudoku solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == num) {
+                return false; // check column
+            }
+            if (board[row][i] == num) {
+                return false; // check row
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == num) {
+                return false; // check 3x3 box
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
